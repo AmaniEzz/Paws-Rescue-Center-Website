0cf07f2e164a56c58cba56d942c185dded4a07dc
AmaniEzz <amanymounas@gmail.com>
2020-07-08 03:08:50 +0300

    Initial Commit

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 """Information regarding the Pets in the System."""
 pets = [
-    {"id": 1, "name": "Nelly", "age": "5 weeks", "bio": "I'm Nelly and I love squeaky toys and cuddles."},
-    {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I'm handsome cat like to dress up in bow ties."},
-    {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
-    {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "I'm an old grandpa, Probably napping."}, 
-    {"id": 5, "name": "Aze", "age": "1 year", "bio":    "I'm a quite cat, i love treats"},
-    {"id": 6, "name": "suna", "age": "2 month", "bio":  "i feel so lucky to be rescued"},
-    {"id": 5, "name": "Aze", "age": "1 year", "bio":    "I'm a quite cat, i love treats"},
-    {"id": 6, "name": "suna", "age": "2 month", "bio":  "i feel so lucky to be rescued"},
+    {"id": 1, "name": "Nelly", "age": "5 weeks",        "bio": "I love squeaky toys and cuddles......."},
+    {"id": 2, "name": "Yuki", "age": "8 months",        "bio": "I'm handsome cat like to dress up in bow ties."},
+    {"id": 3, "name": "Basker", "age": "1 year",        "bio": "I love barking. But, I love my friends more..."},
+    {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "I'm an old grandpa, Probably napping.........."}, 
+    {"id": 5, "name": "Aze", "age": "1 year",           "bio": "I'm a quite cat, i love treats."},
+    {"id": 6, "name": "suna", "age": "2 month",         "bio": "I feel so lucky to be rescued."},
+    {"id": 5, "name": "Aze", "age": "1 year",           "bio": "I'm a quite cat, i love treats."},
+    {"id": 6, "name": "suna", "age": "2 month",         "bio": "i feel so lucky to be rescued."},
 
     ]
 
@@ -62,4 +62,4 @@ def pet_details(pet_ID):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7351)
+    app.run(debug=True, port=6682)
