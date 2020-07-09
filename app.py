8d481c641c86b14eedb23fc6277f62dcff4caf0f
AmaniEzz <amanymounas@gmail.com>
2020-07-09 06:55:37 +0300

    new Commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, abort
-from forms import LoginForm
+from flask import Flask, render_template, abort,session,redirect,url_for
+from forms import LoginForm, SignupForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
+
 
 """Information regarding the Pets in the System."""
 pets = [
@@ -18,11 +19,17 @@ pets = [
 
     ]
 
-users = {
-    "archie.andrews@email.com": "football4life",
-    "veronica.lodge@email.com": "fashiondiva"
-}
-    
+"""Information regarding the Users in the System."""
+
+users = [
+    {"username": "Amany", "password":"ddddddd", "email": "amanymounas@gmail.com"},
+    {"username": "Mona",  "password":"4534366", "email": "Mona.M@email.com"},
+    {"username": "Ahmed", "password":"deh3nb4", "email": "Ahmed1234@gmail.com"},
+    {"username": "Magdy", "password":"uud7dye", "email": "Magdy1234@gmail.com"},
+   
+]
+print(users)
+
 @app.route("/")
 def home():
     return render_template("home.html", pets=pets)
@@ -31,23 +38,46 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        new_user = {"username": form.username.data, "password": form.password.data, "email": form.email.data}
+        users.append(new_user)
+        return render_template("signup.html",  message = "Successfully Signed up")
+
+    return render_template("signup.html", form =form, email_error = form.email.errors, confirm_error =form.confirm.errors)
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-   
     if form.validate_on_submit():
-       print("Submitted and Valid.")
+        for user in users:
+            user = next((user for user in users if user["email"] == form.email.data and user["password"] == form.password.data), None)
+            if user is None:
+                return render_template("login.html", form = form, msg= "Wrong Credentials.")
 
-    elif form.errors:
-        print(form.errors.items())
+            else:
+                session['user'] = user
+                return render_template("user_page.html", msg= "Successfully Logged In")
 
     return render_template("login.html", form = form)
+
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('home'))
+
 
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 @app.route("/details/<int:pet_ID>")
 def pet_details(pet_ID):
--- forms.py
+++ forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired, Email, EqualTo , Length, ValidationError
+
+
+def my_length_check(form, field):
+    if len(field.data) > 50:
+        raise ValidationError('Field must be less than 50 characters')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
+
+
+class SignupForm(FlaskForm):
+    username = StringField('Full name', validators=[InputRequired(), my_length_check])
+    email = StringField('Email', validators=[InputRequired(), Email(message="Invalid Email addresse")])
+    password = PasswordField('Password', validators = [InputRequired()])
+    confirm  = PasswordField('Confirm Password', validators = [InputRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Sign Up')
